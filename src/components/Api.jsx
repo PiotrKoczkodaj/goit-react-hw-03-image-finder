27aa64bca72114479267761b7c804c39f55712e7
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-06-08 00:34:55 +0200

    .

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const fetchData = async (inputValue,page,quantityElements) => {
-    
+export const fetchData = async (inputValue,page,quantityElements) => { 
     const response = await axios.get(`https://pixabay.com/api/?q=${inputValue}&page=${page}&key=34989150-f6282d23d45a19c7980767166&image_type=photo&orientation=horizontal&per_page=${quantityElements}&safesearch=true`);
     return response
 };
